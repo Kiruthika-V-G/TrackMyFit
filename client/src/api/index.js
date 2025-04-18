23ecdd677a5d458164213a03393cf345d1332a9e
Kiruthika V G <21pt10@psgtech.ac.in>
2025-04-18 15:37:04 +0530

    base url fixed

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL : "https://trackmyfit-backend.onrender.com/api",
+    baseURL : process.env.REACT_APP_API_URL,
 })
 
 export const UserSignUp = async(data) => API.post("/user/signup",data);
